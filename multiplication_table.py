8a71073dcd4a6afaaac5b5f5a70e712733c65807
Anto Gracious <31274396+antogracious@users.noreply.github.com>
2020-03-12 15:49:59 +0530

    Add files via upload

---
+++ multiplication_table.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the number for multiplication table:"))
+b=int(input("Enter the steps for multiplication table:"))
+i=int(input("Enter the Starting digit for multiplication table:"))
+print("Multiplication table of",a)
+while(i<=b):
+        print(i,'x',a,'=',a*i)
+        i=i+1
+    
